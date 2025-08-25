7686a792936e902e1dc86610fadc882690c3f220
curtis-murray <murrcurt09@gmail.com>
2025-08-26 00:46:02 +1000

    Add custom keymap with new thumb layout and layers

---
+++ custom_config.h
@@ -0,0 +1,27 @@
+// Custom configuration for Corne V4.1 keymap
+
+#pragma once
+
+// Home row mods settings
+#define TAPPING_TERM 200
+#define TAPPING_TERM_PER_KEY
+
+// Make home row mods more responsive
+#define PERMISSIVE_HOLD
+#define PERMISSIVE_HOLD_PER_KEY
+#define HOLD_ON_OTHER_KEY_PRESS
+#define QUICK_TAP_TERM 0
+
+// Prevent accidental triggers
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// USB settings
+#define USB_POLLING_INTERVAL_MS 1
+#define FORCE_NKRO
+
+// Layer settings
+#define LAYER_STATE_8BIT
+
+// Disable unused features to save space
+#define NO_ACTION_ONESHOT
+#define NO_MUSIC_MODE
